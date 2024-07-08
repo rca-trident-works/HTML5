8f14555173429f78a47412361bf7659b79c2255a
ろむねこ <nem@nem.x0.to>
2024-07-08 10:02:56 +0900

    wip

--- 0708/index.js
+++ 0708/index.js
@@ -1,3 +1,9 @@
 var element = document.getElementById("wether");
 console.log(element);
 element.innerHTML = "<span>曇り</span>"
+
+var element = document.querySelector(".clsWeather");
+console.log(element);
+
+var element = document.querySelector("dl");
+console.log(element);
